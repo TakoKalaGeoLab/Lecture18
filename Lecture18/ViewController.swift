5e222285b44d13800e544374bbf54fdef48b1b5e
Lizi Chichua <83401753+LiziChichua@users.noreply.github.com>
2025-12-17 20:13:53 +0400

    Initial commit

---
+++ Lecture18/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lecture18
+//
+//  Created by Lizi Chichua on 17.12.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
