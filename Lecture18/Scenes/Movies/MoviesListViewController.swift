23e3688b0c032fdc773fbe7e095697bada35dab1
Lizi Chichua <takokala@yahoo.com>
2025-12-17 21:25:55 +0400

    Feature: Added Network Layer

--- Lecture18/Scenes/Movies/MoviesListViewController.swift
+++ Lecture18/Scenes/Movies/MoviesListViewController.swift
@@ -8,12 +8,26 @@
 import UIKit
 
 class MoviesListViewController: UIViewController {
+    
+    private var movieApiManager: MovieAPIManagerProtocol?
 
     override func viewDidLoad() {
-        //super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        super.viewDidLoad()
+        
+        fetchMovies()
     }
 
+    private func fetchMovies() {
+        movieApiManager = MovieAPIManager()
+        
+        movieApiManager?.fetchMovies(with: .upcoming) { result in
+            switch result {
+            case .success(let movieResponse):
+                print(movieResponse.movies)
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
 
 }
-
